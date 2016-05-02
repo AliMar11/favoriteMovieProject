8861b1b9974b8403f0a3c380da269f4da2d220d0
AliMar11 <alicia.marisal@gmail.com>
2016-05-02 18:19:36 -0400

    application MVP classes and storyboard complete

---
+++ favoriteMovieProject/favoriteMovieProject/UIMovieCollectionViewCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  UIMovieCollectionViewCollectionViewController.h
+//  favoriteMovieProject
+//
+//  Created by Alicia Marisal on 5/2/16.
+//  Copyright © 2016 Alicia Marisal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIMovieCollectionViewCollectionViewController : UICollectionViewController
+
+@end
---
+++ favoriteMovieProject/favoriteMovieProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  favoriteMovieProject
+//
+//  Created by Alicia Marisal on 5/2/16.
+//  Copyright © 2016 Alicia Marisal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
